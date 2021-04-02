5e7405710b555ec39f37d9ddc90f7a3356a6a6d9
sommersoft <sommersoft@gmail.com>
2021-04-01 21:50:14 -0500

    one of these dumbs

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-find -path **/*.yml
-
-which python
+echo $(find -path **/*.yml)
 
 cd $GITHUB_WORKSPACE
+
+echo $(pwd)
+
+ls -g
 
 python -m downstream_runner $DS_YAML $DS_JOBS $DS_EXCLUDE
