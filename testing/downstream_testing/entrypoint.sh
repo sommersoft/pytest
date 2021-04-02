f2d34355d8933c275664723a9281a8a008b1784b
sommersoft <sommersoft@gmail.com>
2021-04-01 22:10:49 -0500

    dumbdumbdumb

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-echo $(pwd)
-
-ls -g
-
-python -c "import os; print(os.getcwd())"
-
-python -m downstream_runner $DS_YAML $DS_JOBS $DS_EXCLUDE
+python -m testing.downstream_testing.downstream_runner $DS_YAML $DS_JOBS $DS_EXCLUDE
