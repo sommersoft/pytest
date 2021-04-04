08e55a3e400528dba368b7ea72492fdeebf1175e
sommersoft <sommersoft@gmail.com>
2021-04-04 13:45:27 -0500

    dumbrole

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-groups $USER
+groups root
 groups postgres
 usermod -a -G postgres root
-groups $USER
+groups root
 
 python3.9 -m pip install --no-cache-dir pyyaml
 
 /etc/init.d/postgresql start
 pg_isready
-psql --command "CREATE USER $USER WITH CREATEDB"
+psql --command "CREATE USER $USER WITH CREATEDB CREATEROLE"
 
 python3.9 -u -m testing.downstream_testing.downstream_runner $INPUT_DS_NAME $INPUT_DS_YAML $INPUT_DS_JOBS --matrix-exclude $INPUT_DS_MATRIX_EXCLUDE
