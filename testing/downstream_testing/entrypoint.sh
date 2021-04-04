e16404318973ffdc3120fd879dee921a46cbef8a
sommersoft <sommersoft@gmail.com>
2021-04-03 19:19:34 -0500

    pipdumb

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+python3.9 -m pip install --no-cache-dir pyyaml
+
 python3.9 -u -m testing.downstream_testing.downstream_runner $INPUT_DS_NAME $INPUT_DS_YAML $INPUT_DS_JOBS --matrix-exclude $INPUT_DS_MATRIX_EXCLUDE
