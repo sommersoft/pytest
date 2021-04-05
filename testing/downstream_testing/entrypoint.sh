f29f2ee505e29769320133c2ecf59d17ff22c516
sommersoft <sommersoft@gmail.com>
2021-04-05 15:34:20 -0500

    confusdumb

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#usermod -a -G postgres, ssl-cert root
-
-
 python3.9 -m pip install --no-cache-dir pyyaml
 
 #/etc/init.d/postgresql start
 #pg_isready
 #psql --command "CREATE USER $USER WITH CREATEDB CREATEROLE"
-/usr/lib/postgresql/11/bin/postgres -D /var/lib/postgresql/11/main -c config_file=/var/lib/postgresql/11/main/postgresql.conf
+#/usr/lib/postgresql/11/bin/postgres -D /var/lib/postgresql/11/main -c config_file=/var/lib/postgresql/11/main/postgresql.conf
 
 python3.9 -u -m testing.downstream_testing.downstream_runner $INPUT_DS_NAME $INPUT_DS_YAML $INPUT_DS_JOBS --matrix-exclude $INPUT_DS_MATRIX_EXCLUDE
