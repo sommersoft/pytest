4588746409c33def291923ca38f2888eca8eeb79
sommersoft <sommersoft@gmail.com>
2021-04-05 17:28:00 -0500

    letsgetdumb

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -6,5 +6,6 @@ python3.9 -m pip install --no-cache-dir pyyaml
 #pg_isready
 #psql --command "CREATE USER $USER WITH CREATEDB CREATEROLE"
 #/usr/lib/postgresql/11/bin/postgres -D /var/lib/postgresql/11/main -c config_file=/var/lib/postgresql/11/main/postgresql.conf
+./postgres_entrypoint.sh
 
 python3.9 -u -m testing.downstream_testing.downstream_runner $INPUT_DS_NAME $INPUT_DS_YAML $INPUT_DS_JOBS --matrix-exclude $INPUT_DS_MATRIX_EXCLUDE
