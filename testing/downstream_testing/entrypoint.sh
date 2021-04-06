544f5c59a90ec073801976d246cbd9d48fc186d0
sommersoft <sommersoft@gmail.com>
2021-04-06 00:28:03 -0500

    dumbprogress

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -2,6 +2,6 @@
 
 python3.9 -m pip install --no-cache-dir pyyaml
 
-. /postgres_entrypoint.sh
+/postgres_entrypoint.sh postgres >logfile 2>&1 &
 
 python3.9 -u -m testing.downstream_testing.downstream_runner $INPUT_DS_NAME $INPUT_DS_YAML $INPUT_DS_JOBS --matrix-exclude $INPUT_DS_MATRIX_EXCLUDE
