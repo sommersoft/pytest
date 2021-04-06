1a4f46c2a2586fbbdac4636a2c846596db13d509
sommersoft <sommersoft@gmail.com>
2021-04-05 19:12:09 -0500

    bashdumb

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -4,6 +4,6 @@ python3.9 -m pip install --no-cache-dir pyyaml
 
 ls -l /
 
-./postgres_entrypoint.sh
+/bin/bash /postgres_entrypoint.sh
 
 python3.9 -u -m testing.downstream_testing.downstream_runner $INPUT_DS_NAME $INPUT_DS_YAML $INPUT_DS_JOBS --matrix-exclude $INPUT_DS_MATRIX_EXCLUDE
