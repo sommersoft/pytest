2bcafa06f8e307170b87c2b5031afcea7d0ec543
sommersoft <sommersoft@gmail.com>
2021-04-15 15:07:20 -0500

    feelinglucky

--- testing/downstream_testing/entrypoint.sh
+++ testing/downstream_testing/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 eval "$(pyenv init -)"
-
-python3.9 -m pip install --no-cache-dir pyyaml
-
 for VER in $(cat pyenv-versions.txt); 
 do 
     pyenv shell $VER && pyenv exec pip install ./pytest; 
 done
+pyenv shell --unset
 
+python3.9 -m pip install --no-cache-dir pyyaml
 cd /pytest
-
 python3.9 -u -m testing.downstream_testing.downstream_runner $DS_NAME $DS_YAML $DS_JOBS --matrix-exclude $DS_MATRIX_EXCLUDE
